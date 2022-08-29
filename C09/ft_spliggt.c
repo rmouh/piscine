b6056e3713d82ed3c81fbf54be283235f88f122c
rmouhoub <mouhoubimanelc4b1@gmail.com>
2022-08-29 22:35:52 +0200

    c09

--- C09/ft_spliggt.c
+++ C09/ft_spliggt.c
@@ -23,26 +23,7 @@ int is_sep(char c, char *sep)
 
 }
 
-int count_words(char *str, char *charset)
-{
-    int i;
-    int sep_counter;
-
-    sep_counter = 0;
-    i = 0;
-    while (str[i])
-    {
-        if (is_sep(str[i], charset) && !is_sep(str[i - 1], charset)) //count the words
-            sep_counter++;
-        i++;
-    }
-    printf ("le index:%d\n", i);
-    if (!is_sep(str[i - 1], charset))
-        sep_counter++;
-    return (sep_counter);
-}
-/*
-char *ft_split(char *str, char *charset)
+char **ft_split(char *str, char *charset)
 {
     int i;
     int sep_counter;
@@ -51,18 +32,23 @@ char *ft_split(char *str, char *charset)
     int j;
 
     i = 0;
-    sep_counter = 0;
+    sep_counter = 0; 
     while (is_sep(*str, charset))
         str++;
-    
-    // while (str[i])
-    // {
-    //     if (is_sep(str[i], charset))//replace de chrset
-    //         str[i] ='\t';
-    //     i++;
-    // }
+    while (str[i])
+    {
+        if (is_sep(str[i], charset))//replace de chrset
+            str[i] ='\t';
+        i++;
+    }
     i = 0;
-    sep_counter = count_words(str, charset);
+    while (str[i])
+    {
+
+        if (is_sep(str[i], charset) && !is_sep(str[i - 1], charset)) //count the words
+            sep_counter++;
+        i++;
+    }
     dest = malloc (sizeof(char *) * (sep_counter + 1));
     i = 0;
     j = 0;
@@ -77,8 +63,6 @@ char *ft_split(char *str, char *charset)
         dest[j] = malloc (sizeof(char) * (sep_counter ));//sans +1
         j++;
         word_counter = 0;
-        if (is_sep(str[i], charset))
-            i++;
     }
     i = 0;
     j = 0;
@@ -93,19 +77,15 @@ char *ft_split(char *str, char *charset)
         while (is_sep(str[i], charset))
             i++;
     }
-    
     return (dest);
 
+
+
 }
-*/
 int main(int argc, char *argv[])
 {
-    char *dest;
+    char **dest;
     (void)argc;
-    //dest = ft_split(argv[1], argv[2]);
-    //printf("hello");
-    //printf("%s\n", argv[1]);
-    //printf("%s", dest);
-    //printf("%s", dest[2]);
-    printf("%d",count_words(argv[1], argv[2]));
- }
+    dest = ft_split(argv[1], argv[2]);
+    printf("%s", dest[0]);
+}
